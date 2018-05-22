379849ed7e8711acc636659f240e867f16e97e1c
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-05-22 21:23:35 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,13 +1,14 @@
 
 class EmailParser
   attr_accessor :emails
+  @@list = []
 
   def initialize(emails)
     @emails = emails
   end
 
   def parse
-    @emails.split(/\, |\ /).uniq
-    @emails
+    @@list = @emails.split(/\, |\ /).uniq
+    @@list
   end
 end
